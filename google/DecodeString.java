a3445630a7060628dfa205e738717731a132228b
Wei Tan <wayetan@gmail.com>
2017-03-10 17:11:40 -0800

    no message

---
+++ google/DecodeString.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String decodeString(String s) {
+    	String res = "";
+        Stack<Integer> count = new Stack<Integer>();
+        Stack<String> result = new Stack<String>();
+        int i = 0;
+        while(i < s.length()) {
+        	if(Character.isDigit(s.chatAt(i))) {
+        		int count = 0;
+        		while()
+
+        	}
+        }
+    }
+}
--- google/MissingRanges.java
+++ google/MissingRanges.java
@@ -1,5 +1,22 @@
 public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        
+        List<String> res = new ArrayList<String>();
+        for(int n : nums) {
+        	if(n == Integer.MIN_VALUE) {
+        		lower = n + 1;
+        		continue;
+        	}
+        	if(lower == n - 1) {
+        		// single one missing
+        		res.add("" + lower);
+        	} else if(lower < n - 1) {
+        		res.add(lower + "->" + (n - 1))
+        	}
+        	if(n == Integer.MAX_VALUE) return res;
+        	lower = n + 1;
+        }
+        if(lower == upper) res.add("" + lower) // already move to the last one
+        else if(lower < upper) res.add(lower + "->" + upper);
+        return res;
     }
 }

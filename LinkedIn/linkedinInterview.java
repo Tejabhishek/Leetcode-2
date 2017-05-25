8f5f97054c15d5296875542a402e64607f77c70f
Wei Tan <wayetan@gmail.com>
2017-05-24 17:18:44 -0700

    add

--- LinkedIn/linkedinInterview.java
+++ LinkedIn/linkedinInterview.java
@@ -99,15 +99,171 @@ public boolean isValid(String s) {
 	return stack.isEmpty();
 }
 21. Merge Two Sorted Lists
-
+class ListNode {
+    ListNode next;
+    int val;
+}
+public ListNode merge(ListNode l1, ListNode l2) {
+    if(l1 == null) return l2;
+    if(l2 == null) return l1;
+    ListNode head = new ListNode(0);
+    if(l1.val <= l2.val) {
+        head = l1;
+        merge(l1.next, l2);
+    } else {
+        head = l2;
+        merge(l2.next, l1);
+    }
+    return head;
+}
 23. Merge k Sorted Lists
+public ListNode mergeKLists(ArrayList<ListNode> lists) {
+    if(lists == null || lists.length == 0) {
+        return null;
+    }
+    PriorityQueue<ListNode> heap = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+        public int compare(ListNode n1, ListNode n2) { return n1.val - n2.val; }
+    });
+
+    for(ListNode node: lists) {
+        if(node != null) heap.add(node);
+    }
+
+    ListNode head = new ListNode(0);
+    ListNode curr = head;
+    while(!heap.isEmpty()) {
+        ListNode tmp = heap.poll();
+        if(tmp.next != null) heap.add(tmp.next);
+        curr.next = tmp;
+        curr = curr.next;
+    }
+    return head.next;
+}
 
 31. Next Permutation
 
+
 33. Search in Rotated Sorted Array
+public int search(int[] nums, int target) {
+    if(nums == null || nums.length == 0) return -1;
+    int start = 0, end = nums.length - 1;
+    while(start + 1 < end) {
+        int mid =  start + (end - start) / 2;
+        if(nums[mid] == target) return mid;
+        if(nums[start] < nums[mid]) {
+            if(nums[start] <= target && target < nums[mid]) end = mid;;
+            else start = mid;
+        } else {
+            if(nums[mid] < target && target <= nums[end]) start = mid;
+            else end = mid;
+        }
+    }
+    if(nums[start] == target) return start;
+    if(nums[end] == target) return end;
+    return -1;
+}
+
 34. Search for a Range
+public int[] searchRange(int[] A, int target) {
+    if (A.length == 0) {
+        return new int[]{-1, -1};
+    }
+    
+    int start, end, mid;
+    int[] bound = new int[2]; 
+    
+    // search for left bound
+    start = 0; 
+    end = A.length - 1;
+    while (start + 1 < end) {
+        mid = start + (end - start) / 2;
+        if (A[mid] == target) {
+            end = mid;
+        } else if (A[mid] < target) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    if (A[start] == target) {
+        bound[0] = start;
+    } else if (A[end] == target) {
+        bound[0] = end;
+    } else {
+        bound[0] = bound[1] = -1;
+        return bound;
+    }
+    
+    // search for right bound
+    start = 0;
+    end = A.length - 1;
+    while (start + 1 < end) {
+        mid = start + (end - start) / 2;
+        if (A[mid] == target) {
+            start = mid;
+        } else if (A[mid] < target) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    if (A[end] == target) {
+        bound[1] = end;
+    } else if (A[start] == target) {
+        bound[1] = start;
+    } else {
+        bound[0] = bound[1] = -1;
+        return bound;
+    }
+    
+    return bound;
+}
 39. Combination Sum
-40. Combination Sum II
+public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+    Arrays.sort(candidates);
+    combinationSumHelper(candidates, 0, target, new ArrayList<Integer>(), res);
+    return res;
+}
+
+public void combinationSumHelper(int[] candidates, int start, int target, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> res){
+    if(target < 0 || start < 0 || start >= candidates.length)
+        return;
+    if(target == 0) {
+        ArrayList<Integer> result = new ArrayList<Integer>(path);
+        res.add(result);
+    }else {
+        for(int i = start; i < candidates.length && candidates[i] <= target; i++){
+            path.add(candidates[i]);
+            combinationSumHelper(candidates, i, target - candidates[i], path, res);
+            path.remove(path.size() - 1); //reset the variable.
+        }
+    }
+}
+40. Combination Sum II (No Duplicates)
+public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
+    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+    Arrays.sort(num);
+    combinationSumHelper(candidates, 0, target, new ArrayList<Integer>(), res);
+    return res;
+}
+public void combinationSumHelper(int[] candidates, int start, int target, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> res){
+    if(start < 0 || target < 0)
+        return;
+    if(target == 0) {
+        ArrayList<Integer> result = new ArrayList<Integer>(path);
+        res.add(result);
+    }else{
+        for(int i = start; i < candidates.length && candidates[i] <= target; i++){
+            // skip duplicates
+            if(i > start && candidates[i] == candidates[i - 1])
+                continue;
+            path.add(candidates[i]);
+            combinationSumHelper(candidates, i + 1, target - candidates[i], path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
 46. Permutations
 47. Permutations II
 50. Pow(x, n)
